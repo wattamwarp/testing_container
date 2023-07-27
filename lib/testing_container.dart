f4f94df2e85549b6587763cc3eddb5a79943678c
Padmanabh Wattamwar <wattamwarp874@gmail.com>
2023-07-27 15:36:51 +0530

    Initial commit

---
+++ lib/testing_container.dart
@@ -0,0 +1,17 @@
+library testing_container;
+
+import 'package:flutter/material.dart';
+
+class containerColor extends StatelessWidget {
+  const containerColor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     height: 100,
+     width: 100,
+     color: Colors.red,
+    );
+  }
+}
+
